462843d9a4214ca4bc56ed0a7590ae00e3353758
jnevin7 <144211993+jnevin7@users.noreply.github.com>
2024-07-10 09:09:22 +0200

    movie-reviews

---
+++ frontend/src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AddReview from "./components/add-review";
+import MoviesList from "./components/movies-list";
+import Movie from "./components/movie";
+import Login from "./components/login";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from "react-bootstrap/Navbar";
+
+function App() {
+  const [user, setUser] = React.useState(null);
+
+  async function login(user = null) {
+    setUser(user);
+  }
+
+  async function logout() {
+    setUser(null);
+  }
+
+  return (
+    <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand>Movie Reviews</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link>
+              <Link to="/movies">Movies</Link>
+            </Nav.Link>
+            <Nav.Link>
+              {user ? (
+                <a onClick={logout}>Logout User</a>
+              ) : (
+                <Link to="/login">Login</Link>
+              )}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      
+      <Routes>
+        <Route exact path="/" element={<MoviesList />} />
+        <Route path="/movies" element={<MoviesList />} />
+        <Route path="/movies/:id/review" element={<AddReview user={user} />} />
+        <Route path="/movies/:id" element={<Movie user={user} />} />
+        <Route path="/login" element={<Login login={login} />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
